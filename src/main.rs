c45948094088e023a7a93f41229aba65bbb96d1c
Joao-Felisberto <joaofelisberto@ua.pt>
2022-11-06 11:12:05 +0000

    Project done

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::http::Cookies;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/owl")]
+fn owl(cookies: Cookies) -> String {
+    match cookies.get("flag").map(|c| c.value()) {
+        Some(cookie) => {
+            println!("{cookie}");
+            String::from(cookie)
+        }
+        _ => {
+            println!("FAILURE!");
+            String::from("an error has occured")
+        }
+    }
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
